75b492a0983b1bf807a0866a17aff74e7a438401
stevenhoangduc <hoangducsteven@gmail.com>
2025-02-24 14:14:17 -0800

    changed Brand to Make, change Create car to Search car, added Car Inventory

--- models/user.js
+++ models/user.js
@@ -1,6 +1,31 @@
 // this is just a coment
 
+
+
 const mongoose = require('mongoose');
+
+const carsSchema = mongoose.Schema({
+  image: {
+    type: String, 
+    required: true
+  },
+  addText: {
+    type: String,
+    
+  }, 
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Comment"
+  }],
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User"
+  }
+}, {
+  timestamps: true
+})
+
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -11,6 +36,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
+  cars: [carsSchema]
 });
 
 userSchema.set('toJSON', {
